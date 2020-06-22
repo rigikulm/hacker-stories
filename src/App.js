0e42430ff4ec1c43a30f33fc76cb5012b199b793
Chris Lemler <chris@lemler.org>
2020-06-22 12:50:00 -0700

    Typed something in as a placeholder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ const welcome = {
   title: 'React'
 };
 
+const list [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    ObjectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov',
+    num_comments: 2,
+    points: 5,
+    ObjectID: 1
+  }
+];
+
 function getTitle(title) {
   return title;
 }
@@ -19,6 +38,13 @@ function App() {
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr/>
+
+      {list.map((item) => {
+        return <div>{item.title}</div>
+      });
+      }
     </div>
   );
 }
